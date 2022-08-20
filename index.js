7729adb6597a494a50e49a314811d0d94c82b36c
Allan-Ngugi <allan.ngugi@student.moringaschool.com>
2022-08-20 15:58:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const saturdayFun = (Allan)=>{
+    if(Allan === 'bathe my dog'){
+        return 'This Saturday, I want to bathe my dog!'
+    }
+    return 'This Saturday, I want to roller-skate!' 
+}
+const mondayWork = (Allan) =>{
+    if(Allan === 'work from home'){
+        return 'This Monday, I will work from home.'
+    }
+    return 'This Monday, I will go to the office.'
+}
+const  wrapAdjective =  (closers)=> {
+    return function (Allan = 'special') {
+        return `You are ${closers}${Allan}${closers}!`
+    }
+}
+
